1bb545803b4e56731bb5a78507ea23dff8fa9272
Caelan <caelan.borowiec@pqi.org>
2018-06-11 16:34:34 -0400

    v0.2 Task titles to URL titles implemented
    Added custom filter to intercept URL creation and load the task details
    from Asana if the URL stems from Asana.
    Added function to retrieve and return a task name, or 'false' to support
    the filter above.

--- asana-name-lookup/plugin.php
+++ asana-name-lookup/plugin.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Asana Data Import
-Plugin URI: none yet
+Plugin URI: https://github.com/CaelanBorowiec/YOURLS-Asana-Details-Lookup/
 Description: Load url details from Asana
-Version: 0.1
+Version: 0.2
 Author: Caelan Borowiec
 Author URI: https://github.com/CaelanBorowiec
 */
@@ -31,10 +31,45 @@ function asanalookup_display_page() {
 
   $result = $asana->getData();
 
-  // As Asana API documentation says, when a task is created, 201 response code is sent back so...
   if ($asana->hasError()) {
       echo 'Error while trying to connect to Asana, response code: ' . $asana->responseCode;
   }
   if (isset($result->id))
     echo($result->name);
+}
+
+
+// Hook our custom function into the 'shunt_add_new_link' filter
+yourls_add_filter( 'shunt_get_remote_title', 'asanalookup_get_title');
+
+function asanalookup_get_title($false, $url)
+{
+	if (preg_match("/http.:\/\/app\.asana\.com\/.\/([0-9]{1,})\/([0-9]{1,})/", $url, $matches))
+  {
+    // $matches[0] = input, $matches[1] = project, $matches[2] = task,
+		return getAsanaTaskTitle($matches[2]);
+	}
+	return false;
+}
+
+function getAsanaTaskTitle($taskID) {
+  require_once('asana-api-php-class/asana.php');
+
+  // Set up Asana connection
+  global $asanaPAT;
+  $asana = new Asana([
+      'personalAccessToken' => $asanaPAT
+  ]);
+
+  $asana->getTask($taskID);
+
+  $result = $asana->getData();
+
+  if ($asana->hasError()) {
+      echo 'Error while trying to connect to Asana, response code: ' . $asana->responseCode;
+  }
+  if (isset($result->id))
+    return $result->name;
+
+  return false;
 }
