7e7838f2c38e09bf0dfdabdc93c5d30c19ad2276
Caelan <caelan.borowiec@pqi.org>
2018-06-11 16:57:08 -0400

    Details page added
    Removed debug code from details page.  Added actual details.

--- asana-name-lookup/plugin.php
+++ asana-name-lookup/plugin.php
@@ -19,23 +19,39 @@ function asanalookup_add_page() {
 }
 
 function asanalookup_display_page() {
-  require_once('asana-api-php-class/asana.php');
+  //Print plugin details page.
+  ?>
+  <div class="about">
+    <h2>Asana Details Plugin Overview</h2>
+    <p>This plugin integrates with the task/project management software <a href="https://asana.com/product" target="_blank"><b>Asana</b></a>.
+        The plugin provides a method for YOURLS to retireve task titles and other data from Asana when it would ordinarily only see a login screen.</p>
 
-  // Set up Asana connection
-  global $asanaPAT;
-  $asana = new Asana([
-      'personalAccessToken' => $asanaPAT
-  ]);
+    <h3>Setup:</h3>
+    <p>An Asana API "Personal Access Token" must be created for YOURLS.
+        You may wish to do this with an Asana account dedicated to YOURLS.
+        Note that the account used <b>must</b> be able to access the Asana projects/tasks that are being queried.<br />
+      Please review this page on <a href="https://asana.com/guide/help/api/api#gl-access-tokens" target="_blank"><b>how to create a Persona Access Token.</b></a></p>
 
-  $asana->getTask("687756245553309");
+      <p>After you have created a personal access token, please add it to your config in the format: <em>$asanaPAT="yourtoken";</em></p>
 
-  $result = $asana->getData();
+    <h3>Existing Features</h3>
+    <ul>
+      <li>Task name to link title.</li>
+    </ul>
 
-  if ($asana->hasError()) {
-      echo 'Error while trying to connect to Asana, response code: ' . $asana->responseCode;
-  }
-  if (isset($result->id))
-    echo($result->name);
+    <h3>Planned Features</h3>
+    <ul>
+      <li>Project name to link title.</li>
+    </ul>
+
+    <h3>Credits</h3>
+    <ul>
+      <li>Plugin created by <a href="https://github.com/CaelanBorowiec" target="_blank">Caelan Borowiec</a></li>
+      <li>GitHub user ajimix, for their <a href="https://github.com/ajimix/asana-api-php-class" target="_blank">asana-api-php-class</a></li>
+    </ul>
+
+  </div>
+  <?php
 }
 
 
